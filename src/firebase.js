51dcf351d0136f86ac78bce943635bc113041bcb
Ronak Paul <98758085+ronak-pal1@users.noreply.github.com>
2023-10-08 11:05:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLzhPE5TPL-fulZOC0f4VwW0iBNxNQ2c8",
-    authDomain: "slack-clone-542dc.firebaseapp.com",
-    projectId: "slack-clone-542dc",
-    storageBucket: "slack-clone-542dc.appspot.com",
-    messagingSenderId: "875345950969",
-    appId: "1:875345950969:web:8b37db45a02bd01244ca28"
+    // your credentials
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
